5fdd1ddfca75c6ee7d249501270e4a244be881cb
blaysku <nguyen.van.ngoc-b@sun-asterisk.com>
2023-04-06 08:55:06 +0700

    Initial commit

---
+++ ohmyzsh-ubuntu.plugin.zsh
@@ -0,0 +1,49 @@
+(( $+commands[apt] )) && APT=apt || APT=apt-get
+
+alias upgrade="sudo $APT update -y && sudo $APT upgrade -y && sudo snap refresh && sudo $APT autoclean && sudo $APT autoremove"
+alias disable_keyboard="xinput float 19"
+alias enable_keyboard="xinput reattach 19 3"
+
+# Prints apt history
+# Usage:
+#   apt-history install
+#   apt-history upgrade
+#   apt-history remove
+#   apt-history rollback
+#   apt-history list
+# Based On: https://linuxcommando.blogspot.com/2008/08/how-to-show-apt-log-history.html
+function apt-history() {
+  case "$1" in
+    install)
+      zgrep --no-filename 'install ' $(ls -rt /var/log/dpkg*)
+      ;;
+    upgrade|remove)
+      zgrep --no-filename $1 $(ls -rt /var/log/dpkg*)
+      ;;
+    rollback)
+      zgrep --no-filename upgrade $(ls -rt /var/log/dpkg*) | \
+        grep "$2" -A10000000 | \
+        grep "$3" -B10000000 | \
+        awk '{print $4"="$5}'
+      ;;
+    list)
+      zgrep --no-filename '' $(ls -rt /var/log/dpkg*)
+      ;;
+    *)
+      echo "Parameters:"
+      echo " install - Lists all packages that have been installed."
+      echo " upgrade - Lists all packages that have been upgraded."
+      echo " remove - Lists all packages that have been removed."
+      echo " rollback - Lists rollback information."
+      echo " list - Lists all contains of dpkg logs."
+      ;;
+  esac
+}
+
+# List packages by size
+function apt-list-packages {
+  dpkg-query -W --showformat='${Installed-Size} ${Package} ${Status}\n' | \
+  grep -v deinstall | \
+  sort -n | \
+  awk '{print $1" "$2}'
+}
